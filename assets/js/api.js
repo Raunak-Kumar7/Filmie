4d19570ae3ff8662deb2a55490f1a65a9c943eb1
Raunak-Kumar7 <raunakkumar071201@gmail.com>
2023-08-13 02:13:54 +0530

    Project finished

---
+++ assets/js/api.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// const api_key = '23829a23ce35d4698331b31d4c662bd9';
+const api_key = 'f16b6d84f95d8ac3d06ae5384823f2e4';
+const imageBaseUrl = 'https://image.tmdb.org/t/p/';
+
+/*
+    * fetch data from a server using the 'url' and passes 
+    * the result in JSON data to a 'callback' function, 
+    * along with an optional parameter if has 'optionalParam'
+*/
+
+const fetchDataFromServer = function (url, callback, optionalParam) {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => callback(data, optionalParam));
+}
+export { imageBaseUrl, api_key, fetchDataFromServer };
